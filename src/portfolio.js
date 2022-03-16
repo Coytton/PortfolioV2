285302d8cacb22a6b9d1bacc52be4fba84e00fa7
Coytton <100930157+Coytton@users.noreply.github.com>
2022-03-16 21:50:26 +0100

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -507,7 +507,7 @@ const projectsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "PERFIL.jpeg",
+    profile_image_path: "PERFIL2.jpeg",
     description:
       "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
   },
